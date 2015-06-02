d0218e99661a44131f5929a017fcc695c059fc76
Robert Szmurlo <robert.szmurlo@ee.pw.edu.pl>
2015-06-02 08:37:09 +0200

    Super Resistor!

--- src/main/webapp/js/cs.js
+++ src/main/webapp/js/cs.js
@@ -5,7 +5,9 @@
 var components = [];
 var MM_SELECT = 1;
 var MM_RESISTOR = 2;
-var mouseMode = MM_RESISTOR;
+var MM_CAPACITOR = 3;
+
+var mouseMode = MM_SELECT;
 
 $(document).ready( function() {
     init();
@@ -19,7 +21,7 @@ function init() {
 
         switch (mouseMode) {
             case MM_RESISTOR:
-                addComponent(x,y);
+                addComponent(x,y, MM_RESISTOR);
                 redraw();
                 break;
             case MM_SELECT:
@@ -35,7 +37,7 @@ function init() {
 function selectComponent(x,y) {
     for (var i = 0; i < components.length; i++) {
         var c = components[i];
-        if (c.x - 5 < x && x < c.x + 5 && c.y - 5 < y && y < c.y + 5 )
+        if (c.isInside(x,y))
             c.selected = true;
         else
             c.selected = false;
@@ -66,50 +68,70 @@ function getYFor(ev, el) {
     return y;
 }
 
-function addComponent(x,y) {
-    components.push( {
-       "x": x,
-       "y": y,
-       "selected": false
-    });
+function addComponent(x,y, type) {
+    switch (type) {
+        case MM_RESISTOR:
+            components.push( new Resistor(x,y, "Brak"));
+            break;
+    }
 }
 
 function redraw() {
     var c = $('#schemat');
     var context = document.getElementById("schemat").getContext("2d");
     context.clearRect(0,0, context.canvas.width, context.canvas.height);
-
-/*
-    context.beginPath();
-    context.moveTo(100,100);
-    context.lineTo(120,120);
-    context.closePath();
-    context.stroke();
-*/
     context.save();
 
     for (var i=0; i < components.length; i++) {
-        context.beginPath();
         var c = components[i];
-
-        if (c.selected == true) {
-            context.strokeStyle = "red";
-        } else {
-            context.strokeStyle = "black";
-        }
-        context.moveTo(c.x-4, c.y-4);
-        context.lineTo(c.x+8, c.y-4);
-        context.lineTo(c.x+8, c.y+8);
-        context.lineTo(c.x-4, c.y+8);
-        context.lineTo(c.x-4, c.y-4);
-        context.closePath();
-        context.stroke();
-        if (c.selected == true) {
-            context.fillStyle = "red";
-            context.fill();
-        }
+        c.paint(context);
     }
 
     context.restore();
+}
+
+var Resistor = function(x, y, name) {
+    this.x = x;
+    this.y = y;
+    this.w = 10;
+    this.h = 5;
+    this.name = name;
+    this.selected = false;
+}
+
+Resistor.prototype.isInside = function(x,y) {
+    return (this.x - this.w < x && x < this.x + this.w && this.y - this.h < y && y < this.y + this.h);
+}
+
+Resistor.prototype.paint = function(ctx) {
+    ctx.beginPath();
+
+    if (this.selected == true) {
+        ctx.strokeStyle = "red";
+    } else {
+        ctx.strokeStyle = "black";
+    }
+
+    ox = this.x-this.w;
+    oy = this.y-0;
+    h = this.h;
+    ctx.moveTo(ox, oy);
+    ctx.lineTo(ox+4, oy);
+    ctx.lineTo(ox+6, oy-h);
+    ctx.lineTo(ox+8, oy+h);
+    ctx.lineTo(ox+10, oy-h);
+    ctx.lineTo(ox+12, oy+h);
+    ctx.lineTo(ox+14, oy-h);
+    ctx.lineTo(ox+16, oy);
+    ctx.lineTo(ox+20, oy);
+    ctx.moveTo(ox,oy);
+    ctx.closePath();
+    ctx.stroke();
+/*
+    if (this.selected == true) {
+        ctx.fillStyle = "red";
+        ctx.fill();
+    }
+*/
 
 }
